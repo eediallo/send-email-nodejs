57d704755b7f8eed41202ea2c6017135d9db497b
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 15:12:03 +0100

    feat: update sendEmail function to use SendGrid and improve email handling

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
-export const sendEmail = async (req, res) => {
+export const sendEmailEthereal = async (req, res) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -18,4 +19,23 @@ export const sendEmail = async (req, res) => {
   });
 
   res.json(info);
+};
+
+export const sendEmail = async (req, res) => {
+  // req is unused but required for the route handler
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "elhhabdouldiallo@gmail.com", // Change to your recipient
+    from: "englishopps2022@gmail.com",
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+
+  try {
+    const info = await sgMail.send(msg);
+    res.json(info);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
