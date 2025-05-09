77baa7bf88bd45d3bdeef8accfc910c6e34f01ef
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 14:24:59 +0100

    feat: implement sendEmail function with nodemailer and update dependencies

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,3 +1,21 @@
+import nodemailer from "nodemailer";
+
 export const sendEmail = async (req, res) => {
-  res.send("SEND EMAIL FUNCTION");
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "alivia.farrell@ethereal.email",
+      pass: "Nj9d3Y8brWrttBKyYj",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Daniel Hall" <daniel@gmail.com>',
+    to: "bare@example.com",
+    subject: "Test Email",
+    html: "<b>Sending emails with NodeJs</b>", // HTML body
+  });
+
+  res.json(info);
 };
